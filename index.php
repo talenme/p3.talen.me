c7e0dd612de926c9d4969ba7f2eafb3ce178b8f9
Greg Misicko <gregmmail@gmail.com>
2013-12-07 19:40:54 -0500

    Added hand drawn title from my daughter

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 
 <body>
-    <div id='header'>Fast Math Quiz</div>
+    <div id='header'><img src="images/Lightning_Math_sm.jpg"></div>
     <br>
     <div id='wrapper'>
         <div id='instructions'>How many math problems can you solve in one minute? <button id="startButton">Start!</button></div>
@@ -45,6 +45,9 @@
         There is a known issue with the start button - it does not disable during the SECOND countdown timer. I could not figure 
         out why, and I tried various things to fix it. I could have just removed it, but decided to keep it in. You can click it 
         multiple times and see the countdown kick off repeatedly until the timer clock starts up.
+        <br><br>
+        Updates added after the deadline:<br>
+        After showing to my daughter, she wanted to personalize it with her own hand-drawn title.
     </div>
 
     <audio id="boing" src="wav/boing_y.wav" preload="auto"></audio>
